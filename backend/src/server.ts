8268d488eaeb83d6cd1d90cfe062fe118333fab4
Larissa Araújo <lariaraujo201@gmail.com>
2020-06-03 14:49:58 -0300

    feat: add backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  res.json([
-    'Larissa',
-    'Araújo'
-  ])
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(8000);
